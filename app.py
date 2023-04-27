428fdf5548482d86102fd602a33ca0a6e0a93dcb
Паляниця Олександр Іванович <palianytsia.oi@oblenergo.km.ua>
2023-04-27 16:02:07 +0300

    Ported to Flask

---
+++ app.py
@@ -0,0 +1,19 @@
+import mimetypes
+import os
+from flask import Flask, send_from_directory
+from response.dataHandler import getData
+# from markupsafe import escape
+
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
+
+app = Flask(__name__, static_url_path='/')
+
+@app.route('/')
+def index():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+        'index.html', mimetype='text/html')
+
+@app.route('/data')
+def get_data():
+    return getData()
--- response/dataHandler.py
+++ response/dataHandler.py
@@ -1,46 +1,29 @@
-import json, sqlite3
+import sqlite3
 from sqlite3 import Error
-from response.requestHandler import RequestHandler
 
-class DataHandler(RequestHandler):
-    def __init__(self):
-        super().__init__()
-        self.db_file = 'data/streets.db'
-        self.select_sql = None
+def _connect():
+    conn = None
+    try:
+        conn = sqlite3.connect('data/streets.db')
+    except Error as e:
+         print(e)
+    return conn
 
-    def _connect(self):
-        conn = None
-        try:
-            conn = sqlite3.connect(self.db_file)
-        except Error as e:
-            print(e)
-        return conn
-
-    def find(self, table):
-        if table == 'streets':
-            self.select_sql = '''
+_sql = '''
 select
     id, pos, objtype, old_name, new_name, rename_date, applied
 from streets
 order by pos, old_name
 '''
-            self.setStatus(200)
-            return True
-        else:
-            self.setStatus(403)
-            return False
 
-    def getContents(self):
-        records = []
-        with self._connect() as conn:
-            cur = conn.cursor()
-            cur.execute(self.select_sql)
-            names = [description[0] for description in cur.description]
-            rows = cur.fetchall()
-            for row in rows:
-                rec = {n: row[names.index(n)] for n in names}
-                records.append(rec)
-        self.contents = json.dumps(records)
-
-        self.setContentType("application/json; charset=UTF-8")
-        return self.contents.encode('UTF-8')
+def getData():
+    records = []
+    with _connect() as conn:
+        cur = conn.cursor()
+        cur.execute(_sql)
+        names = [description[0] for description in cur.description]
+        rows = cur.fetchall()
+        for row in rows:
+            rec = {n: row[names.index(n)] for n in names}
+            records.append(rec)
+    return records
