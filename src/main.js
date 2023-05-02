2338292ecb8e929430d4ebdc43561a27f9ec923d
Паляниця Олександр Іванович <palianytsia.oi@oblenergo.km.ua>
2023-05-02 16:04:50 +0300

    Cleand up

--- src/main.js
+++
@@ -1,7 +0,0 @@
-// import * as bootstrap from 'bootstrap';
-import '../scss/main.scss';
-import { createRoot } from 'react-dom/client';
-import StreetTable from './App.js';
-
-const root = createRoot(document.getElementById("app"));
-root.render(<StreetTable/>);
